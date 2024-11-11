759c6f9ac20e56d7edb6981da688890fb331b389
Nathalia Reboucas <nathalia.reboucas@iteris.com.br>
2024-11-11 07:44:40 -0300

    Persistindo eventos de produtos no dynamoDb

---
+++ src/main/java/br/com/reboucas/nathalia/aws_project02/model/ProductEventLog.java
@@ -0,0 +1,92 @@
+package br.com.reboucas.nathalia.aws_project02.model;
+
+import br.com.reboucas.nathalia.aws_project02.enums.EventType;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import org.springframework.data.annotation.Id;
+
+@DynamoDBTable(tableName = "product-events")
+public class ProductEventLog {
+
+    @Id
+    private ProductEventKey productEventKey;
+
+    @DynamoDBTypeConvertedEnum
+    @DynamoDBAttribute(attributeName = "eventType")
+    private EventType eventType;
+
+    @DynamoDBAttribute(attributeName = "productId")
+    private Long productId;
+
+    @DynamoDBAttribute(attributeName = "username")
+    private String username;
+
+    @DynamoDBAttribute(attributeName = "timestamp")
+    private Long timestamp;
+
+    @DynamoDBAttribute(attributeName = "ttl")
+    private Long ttl;
+
+    @DynamoDBHashKey(attributeName = "pk")
+    public String getPk() {
+        return this.productEventKey != null ? this.productEventKey.getPk() : null;
+    }
+
+    public void setPk(String pk) {
+        if (this.productEventKey == null) {
+            this.productEventKey = new ProductEventKey();
+        }
+        this.productEventKey.setPk(pk);
+    }
+
+    @DynamoDBRangeKey(attributeName = "sk")
+    public String getSk() {
+        return this.productEventKey != null ? this.productEventKey.getSk() : null;
+    }
+
+    public void setSk(String sk) {
+        if (this.productEventKey == null) {
+            this.productEventKey = new ProductEventKey();
+        }
+        this.productEventKey.setPk(sk);
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Long getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(Long ttl) {
+        this.ttl = ttl;
+    }
+}
--- src/main/java/br/com/reboucas/nathalia/aws_project02/service/ProductEventConsumer.java
+++ src/main/java/br/com/reboucas/nathalia/aws_project02/service/ProductEventConsumer.java
@@ -2,7 +2,9 @@ package br.com.reboucas.nathalia.aws_project02.service;
 
 import br.com.reboucas.nathalia.aws_project02.model.Envelope;
 import br.com.reboucas.nathalia.aws_project02.model.ProductEvent;
+import br.com.reboucas.nathalia.aws_project02.model.ProductEventLog;
 import br.com.reboucas.nathalia.aws_project02.model.SnsMessage;
+import br.com.reboucas.nathalia.aws_project02.repository.ProductEventLogRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +14,15 @@ import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
+import java.time.Duration;
+import java.time.Instant;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ProductEventConsumer {
     private final ObjectMapper objectMapper;
+    private final ProductEventLogRepository productEventLogRepository;
 
     @JmsListener(destination = "${aws.sqs.queue.product.events.name}")
     public void receiverProductEvent(TextMessage textMessage) throws JMSException, JsonProcessingException {
@@ -28,5 +33,23 @@ public class ProductEventConsumer {
                 envelope.getEventType(),
                 productEvent.getProductId(),
                 snsMessage.getMessageId());
+
+        ProductEventLog productEventLog = buildProductEventlog(envelope, productEvent);
+        productEventLogRepository.save(productEventLog);
+    }
+
+    private ProductEventLog buildProductEventlog(Envelope eveEnvelope, ProductEvent productEvent) {
+        long timestamp = Instant.now().toEpochMilli();
+
+        ProductEventLog productEventLog = new ProductEventLog();
+        productEventLog.setPk(productEvent.getCode());
+        productEventLog.setSk(eveEnvelope.getEventType() + "_" + timestamp);
+        productEventLog.setEventType(eveEnvelope.getEventType());
+        productEventLog.setProductId(productEvent.getProductId());
+        productEventLog.setUsername(productEvent.getUsername());
+        productEventLog.setTimestamp(timestamp);
+        productEventLog.setTtl(Instant.now().plus(Duration.ofMinutes(10)).getEpochSecond());
+
+        return productEventLog;
     }
 }
